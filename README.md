36a16b74b02a243998ae9ba56703d5f21a0901e1
FatBear <91361042+mefatbear@users.noreply.github.com>
2021-09-25 16:19:10 +1000

    Add readme Remove console.log

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bitcoin Lightning Network Node Express Tutorial
+
+Basic tutorial on developing Bitcoin Lightning applications using Node and Express. 
+
+I highly recommend reading the full tutorial to understand how this repo works.
+
+
+## Get Started
+
+1. Install [Polar](https://lightningpolar.com)
+2. Start polar network
+3. Find your LND Connect URI from polar LND node
+4. Update `LND_CONNECT_URL` in `src/index.js`
+5. `yarn`
+6. `yarn start` to start express server.
+7. `curl http://localhost:4444/balance` to see balance
+
+## Follow Us On Twitter
+
+[FatBear](https://www.twitter.com/mefatbear)
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const LndGrpc = require("lnd-grpc");
 
 const PORT = 4444;
-const LND_CONNECT_URL =
-  "lndconnect://127.0.0.1:10001?cert=MIICKDCCAc2gAwIBAgIRALaZUgZ_-nijlq03knLYTF8wCgYIKoZIzj0EAwIwMTEfMB0GA1UEChMWbG5kIGF1dG9nZW5lcmF0ZWQgY2VydDEOMAwGA1UEAxMFYWxpY2UwHhcNMjEwOTI0MDM0NzMxWhcNMjIxMTE5MDM0NzMxWjAxMR8wHQYDVQQKExZsbmQgYXV0b2dlbmVyYXRlZCBjZXJ0MQ4wDAYDVQQDEwVhbGljZTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABPa15TtA8zwKAAhC8MydDKp1yDFQuf-UgZY3HlIhRB0A2p9Qx_SH08r4oSY0vbIM64nsqA9mX9jGlbP-P3aeVx6jgcUwgcIwDgYDVR0PAQH_BAQDAgKkMBMGA1UdJQQMMAoGCCsGAQUFBwMBMA8GA1UdEwEB_wQFMAMBAf8wHQYDVR0OBBYEFOG5Q2fbsqKX_e8vo6lVfI-1BWezMGsGA1UdEQRkMGKCBWFsaWNlgglsb2NhbGhvc3SCBWFsaWNlgg5wb2xhci1uMi1hbGljZYIEdW5peIIKdW5peHBhY2tldIIHYnVmY29ubocEfwAAAYcQAAAAAAAAAAAAAAAAAAAAAYcErBIAAzAKBggqhkjOPQQDAgNJADBGAiEA8HUc83vw79AkgWS5477sCjuDkLSxa_LkMfBy5kWH9coCIQCTl3r6NAIytbQqV2zzQbJxKUUxMRfgrLsdX19cuklD5g&macaroon=AgEDbG5kAvgBAwoQWbgd1P7mrssZkgkto0PsfBIBMBoWCgdhZGRyZXNzEgRyZWFkEgV3cml0ZRoTCgRpbmZvEgRyZWFkEgV3cml0ZRoXCghpbnZvaWNlcxIEcmVhZBIFd3JpdGUaIQoIbWFjYXJvb24SCGdlbmVyYXRlEgRyZWFkEgV3cml0ZRoWCgdtZXNzYWdlEgRyZWFkEgV3cml0ZRoXCghvZmZjaGFpbhIEcmVhZBIFd3JpdGUaFgoHb25jaGFpbhIEcmVhZBIFd3JpdGUaFAoFcGVlcnMSBHJlYWQSBXdyaXRlGhgKBnNpZ25lchIIZ2VuZXJhdGUSBHJlYWQAAAYgyKWs-TLgyekUVQaFL2zktwdhWFJYJX0nugpZoP-rQD8";
+const LND_CONNECT_URL = "CHANGE THIS";
 
 const app = express();
 
@@ -20,8 +19,8 @@ async function initLndClient() {
   await lndClient.connect();
 
   const subscription = await lndClient.services.Lightning.subscribeInvoices();
-  subscription.on('data', (invoice) => {
-    console.log('Lightning Invoice Event');
+  subscription.on("data", (invoice) => {
+    console.log("Lightning Invoice Event");
     console.log(invoice);
   });
 }
@@ -30,7 +29,6 @@ initLndClient();
 app.use(express.json({ limit: "2mb" }));
 
 app.get("/balance", async (_req, res) => {
-  console.log(lndClient.services.Lightning);
   const balance = await lndClient.services.Lightning.walletBalance();
   return res.status(200).json(balance);
 });
@@ -38,4 +36,3 @@ app.get("/balance", async (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
